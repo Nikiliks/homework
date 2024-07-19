e1c96dcaf1d93cf9022c4f375597487fa46d8a81
Nikiliks <nktalpeev@gmail.com>
2024-07-19 23:44:09 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+ # "1st program"
+print (9 ** 0.5 * 5)
+
+# "2st program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3st program"
+a = 1234
+b = 5678
+a2 = a % 1000
+a3 = a2 // 10
+b2 = b % 1000
+b3 = b2 // 10
+print (a3 + b3)
+
+# 4th program"
+a = 13.42 
+b = 42.13
+a2 = int (a // 1)
+b2 = int((b - 42) * 100)
+a3 = int ((a - 13) * 100)
+b3 = int((b // 1))
+print (a2 == b2 or a3 == b3)
